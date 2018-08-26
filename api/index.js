ac99f144c4c690b30b70e3412b30a4abb2065def
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-26 22:32:58 +0700

    Add API Menu

--- api/index.js
+++ api/index.js
@@ -3,8 +3,36 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    const node_env = process.env.NODE_ENV
+    let host = ''
+    if (node_env === 'development') host = 'localhost:3000'
+    else host = 'https://galonku.herokuapp.com'
     res.status(200).send({
-        message: "Welcome to Galonku API"
+        title: "Welcome to Galonku API",
+        content: [{
+            admins: {
+                "show-admins": `${host}/admins`,
+                "register": `${host}/admins/register`,
+                "login": `${host}/admins/login`,
+                "logout": `${host}/admins/logout`,
+                "delete-account": `${host}/admins/delete-account/:id`
+            },
+            merchants: {
+                "show-merchants": `${host}/merchants`,
+                "register": `${host}/merchants`,
+                "login": `${host}/merchants/login`,
+                "logout": `${host}/merchants/logout`,
+                "delete-account": `${host}/merchants/delete-account/:id`,
+                "search-merchants": `${host}/merchants/search?q={store_name}`
+            },
+            users: {
+                "register": `${host}/users/register`,
+                "login": `${host}/users/login`,
+                "logout": `${host}/users/logout`,
+                "delete-account": `${host}/users/delete-account/:id`,
+                "search-users": `${host}/users/search?q={username}`
+            }
+        }]
     });
 });
 
