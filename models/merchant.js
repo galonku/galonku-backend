c71e4b3bfad4a53260aa1cdc19c491ad36d8848b
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-26 20:52:21 +0700

    Modify model files

--- models/merchant.js
+++ models/merchant.js
@@ -2,24 +2,38 @@
 module.exports = (sequelize, DataTypes) => {
   var merchant = sequelize.define('merchant', {
     username: {
+      allowNull: false,
+      unique: true,
       type: DataTypes.STRING(40)
     },
     store_name: {
+      allowNull: false,
+      isAlpha: true,
+      unique: true,
       type: DataTypes.STRING(100)
     },
     email: {
+      allowNull: false,
+      unique: true,
+      isEmail: true,
       type: DataTypes.STRING(100)
     },
     password: {
+      allowNull: false,
       type: DataTypes.STRING
     },
     phone_number: {
+      allowNull: false,
       type: DataTypes.STRING(30)
     },
     identity_number: {
+      allowNull: false,
+      unique: true,
+      isNumeric: true,
       type: DataTypes.STRING(20)
     },
     address: {
+      allowNull: false,
       type: DataTypes.TEXT
     }
   }, {});
