00db44bf4138c46611cb944ca584b70af4e7b593
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-26 20:51:01 +0700

    Add authentication management

--- api/merchant/index.js
+++ api/merchant/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const controller = require('./controller/index')
+const authToken = require('../auth/index')
 
 /* GET home page. */
+router.get('/verifytoken', authToken.verifyToken, authToken.valid)
+
 router.get('/', controller.show)
 router.get('/search', controller.searchMerchants)
-router.get('/login',controller.login)
+router.get('/login', controller.login)
 router.post('/register/', controller.register)
-router.delete('/delete-account/:id', controller.deleteAccount)
+router.delete('/delete-account/:id', authToken.verifyToken, controller.deleteAccount)
 
 module.exports = router;
