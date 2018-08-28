b62d674bbfcce3d7150e0380f3748686efc1c5ff
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-28 07:37:59 +0700

    Fix typo in admin controller

--- api/admin/controller/index.js
+++ api/admin/controller/index.js
@@ -20,7 +20,7 @@ const environment = process.env.NODE_ENV
 let ENV
 if (environment === 'development') ENV = process.env.DEVELOPMENT_JWT_SECRET
 else if (environment === 'production') ENV = process.env.PRODUCTION_JWT_SECRET
- 
+
 const controller = {
     show: async (req, res) => {
         Admin
@@ -52,7 +52,7 @@ const controller = {
     },
 
     login: async (req, res) => {
-        const {username,password} = req.body
+        const { username, password } = req.body
         if (username && password) {
             Admin.findOne({
                 where: {
@@ -60,11 +60,11 @@ const controller = {
                 }
             })
                 .then(admin => {
-                    if(admin){
+                    if (admin) {
                         const { id, fullname, username, email } = admin
                         const token = jwt.sign({
-                                id, fullname,
-                                username, email
+                            id, fullname,
+                            username, email
                         },
                             ENV, {
                                 expiresIn: '1d'
@@ -80,64 +80,66 @@ const controller = {
                                     })
                                 } else {
                                     res.status(417).send({
-                                         message: "Wrong Password!!"
-                                     }) 
-                                 }
+                                        message: "Wrong Password!!"
+                                    })
+                                }
                             })
-                    }else res.status(404).send({ message: "Sorry, username and password doesnt exist. Please register before login!" })
+                    } else res.status(404).send({ message: "Sorry, username and password doesnt exist. Please register before login!" })
                 }).catch(err => res.status(400).send(err))
         } else {
             res.status(400).send({
                 message: "Username or Password is wrong"
             })
+        }
+    },
+    
+    updateProfile: (req, res) => {
+        const { id } = req.params;
+        const { password, email } = req.body;
 
-  updateProfile: (req, res) => {
-    const { id } = req.params;
-    const { password, email } = req.body;
+        if (id) {
+            if (password && email) {
+                Admin.update(
+                    {
+                        password,
+                        email,
+                        updatedAt: new Date() + 7
+                    },
+                    { where: { id: id } }
+                ).then(() => {
+                    res.status(200).send({
+                        message: "Successfully update your profile!"
+                    });
+                });
+            } else {
+                res.status(417).send({
+                    message: "Please specify password field and email!"
+                });
+            }
+        } else {
+            res.status(417).send({
+                message: "Please specify Admin ID!"
+            });
+        }
+    },
 
-    if (id) {
-      if (password && email) {
-        Admin.update(
-          {
-            password,
-            email,
-            updatedAt: new Date() + 7
-          },
-          { where: { id: id } }
-        ).then(() => {
-          res.status(200).send({
-            message: "Successfully update your profile!"
-          });
+    logout: (req, res) => {
+        res.status(200).send({
+            message: "Successfully Logout!"
         });
-      } else {
-        res.status(417).send({
-          message: "Please specify password field and email!"
-        });
-      }
-    } else {
-      res.status(417).send({
-        message: "Please specify Admin ID!"
-      });
-    }
-  },
+    },
 
-  logout: (req, res) => {
-    res.status(200).send({
-      message: "Successfully Logout!"
-    });
-  },
-
-  deleteAccount: (req, res) => {
-    const id = Number(req.params.id);
-    const { password } = req.body;
-    if (id) {
-      password
-        ? Admin.destroy({ where: { id: id } })
-        : res.send({ message: "Please specify the password!" });
-    } else {
-      res.status(417).send({ message: "Please specify Admin ID!" });
+    deleteAccount: (req, res) => {
+        const id = Number(req.params.id);
+        const { password } = req.body;
+        if (id) {
+            password
+                ? Admin.destroy({ where: { id: id } })
+                : res.send({ message: "Please specify the password!" });
+        } else {
+            res.status(417).send({ message: "Please specify Admin ID!" });
+        }
     }
-  }
 };
 
 module.exports = controller;
