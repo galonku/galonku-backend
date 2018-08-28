c939d2dcec5eefbbf5785c3cfd9379643921c4ab
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-28 13:42:12 +0700

    Add createdAt and updatedAt in Admins seed

--- seeders/20180828030838-admins.js
+++ seeders/20180828030838-admins.js
@@ -4,32 +4,36 @@ const saltRounds = 10;
 const myPassword = 'superpower';
 const myPassword2 = 'tes123';
 
-const doEncryptPassword =  (textPassword, salt = 5) => {
-    return new Promise((resolve, reject) => {
-        bcrypt.hash(myPassword, salt).then(result => {
-            resolve(result);
-        })
+const doEncryptPassword = (textPassword, salt = 5) => {
+  return new Promise((resolve, reject) => {
+    bcrypt.hash(myPassword, salt).then(result => {
+      resolve(result);
     })
+  })
 }
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {    
+  up: async (queryInterface, Sequelize) => {
     const admins = [{
       "username": "nukesmith",
       "fullname": "Nuke Smith",
-      "email": "nukesmith@mail.com"
-    },{
+      "email": "nukesmith@mail.com",
+      createdAt: new Date() + 7,
+      updatedAt: new Date() + 7
+    }, {
       "username": "paul",
       "fullname": "paul jenkins",
-      "email": "pauljenkins@mail.com"
+      "email": "pauljenkins@mail.com",
+      createdAt: new Date() + 7,
+      updatedAt: new Date() + 7
     }]
 
     await doEncryptPassword(myPassword, saltRounds).then(newPassword => {
-      admins[0].password = newPassword      
+      admins[0].password = newPassword
     })
 
     await doEncryptPassword(myPassword2, saltRounds).then(newPassword => {
-      admins[1].password = newPassword      
+      admins[1].password = newPassword
     })
 
     return queryInterface.bulkInsert('admins', admins, {});
