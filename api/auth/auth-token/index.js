87bb48e47406499ec8a6fb9f3455f20e6774676b
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-29 11:23:08 +0700

    Add verifyToken

--- api/auth/auth-token/index.js
+++ api/auth/auth-token/index.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const models = require('../../../models/index')
+const Merchant = models.merchant
 
 const controller = {
     valid: async (req, res) => {
@@ -7,18 +9,44 @@ const controller = {
 
     verifyToken: async (req, res, next) => {
         const DevToken = req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer'
-        if (DevToken) {    
-                const token = req.headers.authorization.split(' ')[1] || ''
-                jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-                    if (error) {
-                        res.send(417).send({
-                            message: "Token is invalid"
+        if (DevToken) {
+            const token = req.headers.authorization.split(' ')[1] || ''
+            jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+                if (error) {
+                    res.status(417).send({
+                        message: "Token is invalid"
+                    })
+                } else {
+                    req.decoded = decoded
+                    next()
+                }
+            })
+        } else {
+            res.status(400).send({
+                message: "Please specify your Developer token in request headers"
+            })
+        }
+    },
+
+    verifyTokenAsMerchants: async (req, res, next) => {
+        const DevToken = req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer'
+        if (DevToken) {
+            const token = req.headers.authorization.split(' ')[1] || ''
+            jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+                if (error) {
+                    res.status(417).send({
+                        message: "Token is invalid"
+                    })
+                } else {
+                    req.decoded = decoded
+                    let username = decoded.username
+                    Merchant.findAll({ where: { username } })
+                        .then(Response => {
+                            if (Array.length === 0) res.status(417).send({ message: "Sorry, this is not merchants token" })
+                            else next()
                         })
-                    }else{
-                        req.decoded = decoded
-                        next()
-                    }
-                })
+                }
+            })
         } else {
             res.status(400).send({
                 message: "Please specify your Developer token in request headers"
