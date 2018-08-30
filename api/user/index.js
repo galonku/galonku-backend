2090d959ef24860401992fba787dd18695a6bc2f
Wondo Indra <wondoindra28@gmail.com>
2018-08-30 10:32:25 +0700

    added admin role and verify login

--- api/user/index.js
+++ api/user/index.js
@@ -4,21 +4,21 @@ const router = express.Router();
 const controller = require("./controller/index");
 const authToken = require("../auth/auth-token");
 
-router.get("/verifytoken", authToken.verifyToken, authToken.valid);
+router.get("/verifytoken", authToken.verifyTokenAsUser, authToken.valid);
 
-router.get("/", authToken.verifyToken, controller.show);
+router.get("/", authToken.verifyTokenAsUser, controller.show);
 router.get("/search", controller.searchUser);
 router.get("/logout", controller.logout);
 router.put(
   "/edit-profile/:id",
-  authToken.verifyToken,
+  authToken.verifyTokenAsUser,
   controller.updateProfile
 );
 router.post("/login", authToken.verifyLogin, controller.login);
 router.post("/register", controller.register);
 router.delete(
   "/delete-account/:id",
-  authToken.verifyToken,
+  authToken.verifyTokenAsUser,
   controller.deleteAccount
 );
 
