ecbe1e237d33c1f9857ee70308fd9ee5acf3be83
Wondo Indra <wondoindra28@gmail.com>
2018-08-30 15:40:23 +0700

    remove admin register

--- api/admin/index.js
+++ api/admin/index.js
@@ -7,7 +7,6 @@ const authToken = require("../auth/auth-token");
 router.get("/verifytoken", authToken.verifyTokenAsAdmin, authToken.valid);
 router.get("/", authToken.verifyTokenAsAdmin, controller.show);
 router.post("/login", authToken.verifyLogin, controller.login);
-router.post("/register", controller.register);
 router.get("/logout", controller.logout);
 
 module.exports = router;
