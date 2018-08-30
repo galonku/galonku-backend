fc8ad0375b52803beff707b0a004a751d15138c4
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-30 20:04:54 +0700

    Add some endpoint to API Menu

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   const node_env = process.env.NODE_ENV;
   let host = "";
   if (node_env === "development") host = "localhost:3000";
@@ -13,29 +13,34 @@ router.get("/", function(req, res, next) {
       {
         admins: {
           "show-admins": `${host}/admins`,
-          register: `${host}/admins/register`,
-          login: `${host}/admins/login`,
-          logout: `${host}/admins/logout`,
+          "login": `${host}/admins/login`,
+          "logout": `${host}/admins/logout`,
+          "verify-token": "${host}/admins/verifytoken",
           "delete-account": `${host}/admins/delete-account/:id`
         },
         merchants: {
           "show-merchants": `${host}/merchants`,
-          register: `${host}/merchants/register`,
-          login: `${host}/merchants/login`,
-          logout: `${host}/merchants/logout`,
-          "delete-account": `${host}/merchants/delete-account/:id`,
-          "search-merchants": `${host}/merchants/search?q={store_name}`
+          "register": `${host}/merchants/register`,
+          "login": `${host}/merchants/login`,
+          "logout": `${host}/merchants/logout`,
+          "verify-token": `${host}/merchants/verifytoken`,
+          "search-merchants": `${host}/merchants/search?q={store_name}`,
+          "edit-profile": `${host}/merchants/edit-profile/:id`,
+          "show-reviews": `${host}/merchants/reviews`
         },
         users: {
-          register: `${host}/users/register`,
-          login: `${host}/users/login`,
-          logout: `${host}/users/logout`,
+          "register": `${host}/users/register`,
+          "login": `${host}/users/login`,
+          "logout": `${host}/users/logout`,
+          "verify-token": `${host}/users/verifytoken`,
+          "edit-profile": `${host}/users/edit-profile/:id`,
           "delete-account": `${host}/users/delete-account/:id`,
-          "search-users": `${host}/users/search?q={username}`
+          "search-users": `${host}/users/search?q={username}`,
+          "add-reviews": `${host}/merchants/add-reviews`
         },
         orders: {
           "show-oders": `${host}/orders`,
-          order: `${host}/orders/order`
+          "order": `${host}/orders/order`
         }
       }
     ]
