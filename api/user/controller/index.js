4295d25b5468cf1e446a233abd6e8341669c835c
Arie Brainware <jtucke22@mail.ccsf.edu>
2018-08-30 09:55:17 +0700

    Validate token when seach users and remove password field in response

--- api/user/controller/index.js
+++ api/user/controller/index.js
@@ -7,7 +7,7 @@ const Logging = models.logging;
 
 const controller = {
   show: (req, res) => {
-    User.findAll().then(data => res.status(200).send(data));
+    User.findAll({ attributes: { exclude: ['password'] } }).then(data => res.status(200).send(data));
   },
 
   searchUser: async (req, res) => {
@@ -17,10 +17,13 @@ const controller = {
 
     if (keyword) {
       User.findAll({
+        attributes:{
+          exclude:['password']
+        },
         where: {
           username: {
             [op.like]: `%${keyword}%`
-          }
+          }  
         }
       }).then(user => {
         if (user) {
--- api/user/index.js
+++ api/user/index.js
@@ -5,9 +5,10 @@ const controller = require("./controller/index");
 const authToken = require("../auth/auth-token");
 
 router.get("/verifytoken", authToken.verifyToken, authToken.valid);
+router.get("/verifylogin", authToken.verifyLogin)
 
 router.get("/", authToken.verifyToken, controller.show);
-router.get("/search", controller.searchUser);
+router.get("/search", authToken.verifyToken, controller.searchUser);
 router.get("/logout", controller.logout);
 router.put(
   "/edit-profile/:id",
