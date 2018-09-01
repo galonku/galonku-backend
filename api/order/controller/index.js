78e85cb4ca87310760b7ac173cb5afb319ec6d09
Wondo Indra <wondoindra28@gmail.com>
2018-09-01 12:41:17 +0700

    fix get order by id equals to merchant's order and added idorder to orders

--- api/order/controller/index.js
+++ api/order/controller/index.js
@@ -31,7 +31,7 @@ const controller = {
               "notes",
               "store_name",
               "price",
-              "quantity",
+              "quantities",
               "Total",
               "status"
             ],
@@ -55,7 +55,7 @@ const controller = {
       iduser,
       idmerchants,
       merchant,
-      quantity,
+      quantities,
       phone_number,
       user_address,
       user_notes
@@ -67,7 +67,7 @@ const controller = {
             iduser,
             idmerchants,
             merchant,
-            quantity,
+            quantities,
             phone_number,
             user_address,
             user_notes,
@@ -97,10 +97,55 @@ const controller = {
   },
 
   showOrderById: (req, res) => {
-    const { id } = req.params
-    if(id){
-      Order.findById(id)
-      .then(orders=>res.status(200).send(orders))
+    const { id } = req.params;
+    const sequelize = require("sequelize");
+    const op = sequelize.Op;
+    const DevToken =
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer";
+    if (DevToken) {
+      const token = req.headers.authorization.split(" ")[1] || "";
+      jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+        if (error) {
+          res.status(417).send({
+            message: "Token is invalid"
+          });
+        } else {
+          req.decoded = decoded;
+          let fullname = decoded.ufullname;
+          let store_name = decoded.mstore_name;
+          Vieworder.findOne({
+            attributes: [
+              "idorder",
+              "fullname",
+              "address",
+              "phone number",
+              "notes",
+              "store_name",
+              "price",
+              "quantities",
+              "Total",
+              "status"
+            ],
+            where: {
+              [op.or]: [{ fullname: fullname }, { store_name: store_name }],
+              idorder: id
+            }
+          }).then(data => {
+            if (data) {
+              res.status(200).send(data);
+            } else {
+              res.status(404).send({
+                message: "You dont have order by that id"
+              });
+            }
+          });
+        }
+      });
+    } else {
+      res.status(400).send({
+        message: "Please specify the token in request headers"
+      });
     }
   }
 };
